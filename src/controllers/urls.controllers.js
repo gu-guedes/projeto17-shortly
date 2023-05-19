429c748d230b7541718bc77bf1ecb077b64e96cb
gu-guedes <guguedes2020@gmail.com>
2023-05-19 16:15:35 -0300

    teste do post urls/shorten, get by id e get open link

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -20,9 +20,26 @@ export async function getUrls(req, res){
     const {id} = req.params
     try{
         const urlById = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id])
-        if(!urlById.rows[0].shortUrl) return res.sendStatus(404)
+        if(!urlById.rows[0]) return res.sendStatus(404)
         const result = {"id": urlById.rows[0].id, "shortUrl": urlById.rows[0].shortUrl, "url": urlById.rows[0].url}
         res.status(200).send(result)
+
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
+export async function openLinkUrl(req, res){
+    const {shortUrl} = req.params
+    console.log(shortUrl)
+    try{
+        const result = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl])
+        if(!result.rows[0]) return res.sendStatus(404)
+        const updateVisitCount = result.rows[0].visitCount + 1
+        
+        await db.query(`UPDATE urls SET "visitCount" = $1 WHERE id = $2`,[updateVisitCount, result.rows[0].id])
+
+        res.redirect(result.rows[0].url)
+
 
     }catch(err){
         res.status(500).send(err.message)
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,12 +1,13 @@
 import {Router } from "express"
 import validateSchema from "../middlewares/validateSchemas.middlewares.js"
 import { urlSchema } from "../schemas/urls.schemas.js"
-import { createShortUrl, getUrls } from "../controllers/urls.controllers.js"
+import { createShortUrl, getUrls, openLinkUrl } from "../controllers/urls.controllers.js"
 import validateAuth from "../middlewares/validateAuth.middlewares.js"
 
 const urlsRouter = Router()
 
 urlsRouter.post("/urls/shorten", validateSchema(urlSchema), validateAuth, createShortUrl)
 urlsRouter.get("/urls/:id", getUrls)
+urlsRouter.get("/urls/open/:shortUrl", openLinkUrl)
 
 export default urlsRouter
