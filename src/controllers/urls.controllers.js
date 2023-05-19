270d2e8ac00386a2b37ed6b8b5d075fb6151f588
gu-guedes <guguedes2020@gmail.com>
2023-05-19 15:32:19 -0300

    versao para teste2 post /url/shorten + get by id

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -16,3 +16,15 @@ export async function createShortUrl(req, res) {
         res.status(500).send(err.message)
     }
 }
+export async function getUrls(req, res){
+    const {id} = req.params
+    try{
+        const urlById = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id])
+        if(!urlById.rows[0].shortUrl) return res.sendStatus(404)
+        const result = {"id": urlById.rows[0].id, "shortUrl": urlById.rows[0].shortUrl, "url": urlById.rows[0].url}
+        res.status(200).send(result)
+
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
