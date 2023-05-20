df1abff030547f17521aeab26229b20d1aa5cbd7
gu-guedes <guguedes2020@gmail.com>
2023-05-20 10:21:06 -0300

    rota users/me implementada

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -10,54 +10,83 @@ export async function createShortUrl(req, res) {
 
         await db.query(`INSERT INTO urls (url, "shortUrl", "userId" ) VALUES ($1, $2, $3);`, [url, shortUrl, userId])
 
-        const result = {"id": userId, "shortUrl": shortUrl}
+        const result = { "id": userId, "shortUrl": shortUrl }
         res.status(201).send(result)
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
-export async function getUrls(req, res){
-    const {id} = req.params
-    try{
+export async function getUrls(req, res) {
+    const { id } = req.params
+    try {
         const urlById = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id])
-        if(!urlById.rows[0]) return res.sendStatus(404)
-        const result = {"id": urlById.rows[0].id, "shortUrl": urlById.rows[0].shortUrl, "url": urlById.rows[0].url}
+        if (!urlById.rows[0]) return res.sendStatus(404)
+        const result = { "id": urlById.rows[0].id, "shortUrl": urlById.rows[0].shortUrl, "url": urlById.rows[0].url }
         res.status(200).send(result)
 
-    }catch(err){
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
-export async function openLinkUrl(req, res){
-    const {shortUrl} = req.params
+export async function openLinkUrl(req, res) {
+    const { shortUrl } = req.params
     console.log(shortUrl)
-    try{
+    try {
         const result = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl])
-        if(!result.rows[0]) return res.sendStatus(404)
+        if (!result.rows[0]) return res.sendStatus(404)
         const updateVisitCount = result.rows[0].visitCount + 1
-        
-        await db.query(`UPDATE urls SET "visitCount" = $1 WHERE id = $2`,[updateVisitCount, result.rows[0].id])
+
+        await db.query(`UPDATE urls SET "visitCount" = $1 WHERE id = $2`, [updateVisitCount, result.rows[0].id])
 
         res.redirect(result.rows[0].url)
 
 
-    }catch(err){
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
-export async function deleteUrl(req, res){
-    const {id} = req.params
+export async function deleteUrl(req, res) {
+    const { id } = req.params
     const { userId } = res.locals.user
-    try{
+    try {
         const urlById = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id])
-        if(!urlById.rows[0]) return res.sendStatus(404)
+        if (!urlById.rows[0]) return res.sendStatus(404)
         const result = await db.query(`SELECT * FROM urls WHERE id = $1 AND "userId" = $2`, [id, userId])
-        if(!result.rows[0]) return res.sendStatus(401)
+        if (!result.rows[0]) return res.sendStatus(401)
         await db.query(`DELETE FROM urls WHERE id = $1;`, [id])
         res.sendStatus(204)
 
-    }catch(err){
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+export async function getUserMe(req, res) {
+    const {userId} = res.locals.user
+    try {
+        const result = await db.query(`
+        SELECT
+        users.id,
+        users.name,
+        SUM(urls."visitCount") AS "visitCount",
+        ARRAY_AGG(
+          JSON_BUILD_OBJECT(
+            'id', urls.id,
+            'shortUrl', urls."shortUrl",
+            'url', urls.url,
+            'visitCount', urls."visitCount"
+          )
+        ) AS "shortenedUrls"
+      FROM
+        users
+      LEFT JOIN
+        urls ON users.id = urls."userId"
+      WHERE
+        users.id = $1
+      GROUP BY
+        users.id;`, [userId])
+        res.status(200).send(result.rows)
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,7 +1,7 @@
 import {Router } from "express"
 import validateSchema from "../middlewares/validateSchemas.middlewares.js"
 import { urlSchema } from "../schemas/urls.schemas.js"
-import { createShortUrl, deleteUrl, getUrls, openLinkUrl } from "../controllers/urls.controllers.js"
+import { createShortUrl, deleteUrl, getUrls, getUserMe, openLinkUrl } from "../controllers/urls.controllers.js"
 import validateAuth from "../middlewares/validateAuth.middlewares.js"
 
 const urlsRouter = Router()
@@ -10,5 +10,6 @@ urlsRouter.post("/urls/shorten", validateSchema(urlSchema), validateAuth, create
 urlsRouter.get("/urls/:id", getUrls)
 urlsRouter.get("/urls/open/:shortUrl", openLinkUrl)
 urlsRouter.delete("/urls/:id", validateAuth, deleteUrl)
+urlsRouter.get("/users/me", validateAuth, getUserMe)
 
 export default urlsRouter
