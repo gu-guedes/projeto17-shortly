7c49fd63837c6162434b697e85ccf129221d401c
gu-guedes <guguedes2020@gmail.com>
2023-05-22 12:26:58 -0300

    versao final clean code

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
-import { getUsers, signIn, signUp } from "../controllers/users.controllers.js"
+import { signIn, signUp } from "../controllers/users.controllers.js"
 import validateSchema from "../middlewares/validateSchemas.middlewares.js"
 import { signinSchema, signupSchema } from "../schemas/users.schemas.js"
 
 const usersRouter = Router()
 
-usersRouter.get("/users", getUsers)
+
 usersRouter.post("/signup", validateSchema(signupSchema), signUp)
 usersRouter.post("/signin", validateSchema(signinSchema), signIn)
 
